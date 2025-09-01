3df42abae07252e9bc717372ccc5aa892ae01d71
Aman Singh <amxnsingh07@gmail.com>
2025-09-02 00:51:07 +0530

    commit changes

--- Text Input/Textinput.java
+++ Text Input/Textinput.java
@@ -1,5 +1,6 @@
+
 import java.util.Scanner;
-public class Textinput {
+public class Textinput{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
--- com/abhishek/Main.java
+++ com/abhishek/Main.java
@@ -1,5 +1,10 @@
 package com.abhishek;
-
+import java.util.Scanner;
 public class Main {
-    
+      public static void main(String[] args) {
+            try (Scanner input = new Scanner(System.in)) {
+                  System.out.println(input.nextLine());
+            }
+            
+      }
 }
