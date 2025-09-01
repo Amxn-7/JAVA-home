4cbb9bf080984549048b9675792068a3d1e57a7a
Aman Singh <amxnsingh07@gmail.com>
2025-09-01 10:34:19 +0530

    how to take input

--- Main.java
+++ Main.java
@@ -23,3 +23,4 @@ public class Main {
     s.studentGrade(95);
     }
 }
+
---
+++ Textinput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Textinput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, "+ name);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.println("Your age: " + age);
+        double gpa = scanner.nextDouble();
+        System.out.println("Your cgpa: " + gpa);
+        scanner.close();
+    }
+}
