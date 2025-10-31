c3966620a67ba84bd0c44626ad86f520366162a8
Aman Singh <amxnsingh07@gmail.com>
2025-10-31 09:36:59 +0530

    add insertion sorting

---
+++ Sorting/Inerstion/Main.java
@@ -0,0 +1,20 @@
+package Sorting.Inerstion;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {5,6,4,3,2};
+        for(int i=1;i<arr.length;i++){
+            int temp = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j]>temp){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+    }
+}
+ 
